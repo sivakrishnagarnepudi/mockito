e029f797d4e1b5daa327baf48b9a03b706fb79c7
sivakrishnagarnepudi <gsivakrishna007@gmail.com>
2024-05-29 16:40:15 +0530

    new commit1

--- untitled10/Solar.java
+++ untitled10/Solar.java
@@ -2,6 +2,7 @@ public class Solar {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("new Commit");
+        System.out.println("last commit");
     }
 }
 //hi
