cd3415a25a3a46a9801f670186666a379022d363
SivakrishnaGarnepudi07 <sgarnepudi@neoterictechnologiesinc.com>
2024-07-05 11:04:26 +0530

    creation of folderstructure

---
+++ nss-rec/recruitment/src/test/java/com/nss/recruitment/recruitment/RecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nss.recruitment.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
